1885a20cde3d35c1f37d5f1c58fc6a479f30a52f
lejeunemax <max-lej@hotmail.com>
2025-12-16 15:47:01 +0100

    Adding a coupling with Floris

--- wifa/main_api.py
+++ wifa/main_api.py
@@ -11,16 +11,20 @@ from .cs_api.cs_modules.csLaunch.cs_run_function import run_code_saturne
 from .foxes_api import run_foxes
 from .pywake_api import run_pywake
 from .wayve_api import run_wayve
+from .floris_api import run_floris
 
 sys.path.append(windIO.__path__[0])
 
 
 def run_api(yaml_input):
     # validate input
-    validate_yaml(yaml_input, windIO.__path__[0] + "/plant/wind_energy_system.yaml")
+    validate_yaml(yaml_input, "/root/code/libs/windIO_flow/windIO/schemas/plant/wind_energy_system")
 
     # get number of turbines
-    yaml_dat = load_yaml(yaml_input)
+    if isinstance(yaml_input, dict):
+        yaml_dat = yaml_input
+    else:
+        yaml_dat = load_yaml(yaml_input)
 
     model_name = yaml_dat["attributes"]["flow_model"]["name"]
 
@@ -29,6 +33,9 @@ def run_api(yaml_input):
 
     elif model_name.lower() == "foxes":
         foxes_aep = run_foxes(yaml_input)
+
+    elif model_name.lower() == "floris":
+        floris_aep = run_floris(yaml_input)
 
     elif model_name.lower() == "wayve":
         # Output directory
