26057506b498d02544dc4f042c8be1f478d1949d
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2026-01-26 08:52:35 +0100

    updating foxes tests (#43)

--- tests/test_foxes.py
+++ tests/test_foxes.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
+from shutil import rmtree
 
-from foxes import Engine, reset_engine
 from windIO import __path__ as wiop
 from windIO import validate as validate_yaml
 
@@ -10,30 +10,17 @@ from wifa.foxes_api import run_foxes
 test_path = Path(os.path.dirname(__file__))
 windIO_path = Path(wiop[0])
 
-engine = None
-
 
 def _run_foxes(wes_dir):
     assert wes_dir.is_dir(), f"{wes_dir} is not a directory"
 
-    global engine
-    if engine is None:
-        engine = Engine.new("default", verbosity=0)
-        engine.initialize()
-        print("SETTING ENGINE:", engine)
-
     for yaml_input in wes_dir.glob("system.yaml"):
-        if "_noXYgrid" not in str(yaml_input):
-            print("\nRUNNING FOXES ON", yaml_input, "\n")
-            validate_yaml(yaml_input, Path("plant/wind_energy_system"))
-            output_dir_name = Path("output_test_foxes")
-            output_dir_name.mkdir(parents=True, exist_ok=True)
-            try:
-                run_foxes(yaml_input, output_dir=output_dir_name, engine=None)
-            except Exception as e:
-                reset_engine()
-                engine = None
-                raise e
+        print("\nRUNNING FOXES ON", yaml_input, "\n")
+        validate_yaml(yaml_input, Path("plant/wind_energy_system"))
+        output_dir_name = Path("output_test_foxes")
+        output_dir_name.mkdir(parents=True, exist_ok=True)
+        run_foxes(yaml_input, output_dir=output_dir_name)
+        rmtree(output_dir_name)
 
 
 def test_foxes_KUL():
